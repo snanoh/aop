a04c0c4612362da97df0d5123ebbe6f04d6c1dd8
snanoh <njs1324@gmail.com>
2022-06-06 14:42:04 +0900

    6.05  - aop exam

---
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -0,0 +1,24 @@
+package hello.aop.exam;
+
+import hello.aop.exam.annotation.Trace;
+import hello.aop.exam.aop.TraceAspect;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Import(TraceAspect.class)
+public class ExamRepository {
+    private static int seq = 0;
+
+    /*
+    * 5번에 1번 실패하는 요청
+    * */
+    @Trace
+    public String save(String itemId) {
+        seq++;
+        if (seq % 5 == 0) {
+            throw new IllegalStateException("예외 발생");
+        }
+        return "ok";
+    }
+}
---
+++ src/main/java/hello/aop/exam/ExamService.java
@@ -0,0 +1,20 @@
+package hello.aop.exam;
+
+import hello.aop.exam.annotation.Trace;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExamService {
+    private final ExamRepository examRepository;
+
+    public ExamService(ExamRepository examRepository) {
+        this.examRepository = examRepository;
+    }
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+
+
+}
